54f2995b5f84e94d0599b5a15650be45a7970aff
Manuel Palmeira <mnp.manuel@gmail.com>
2017-01-07 01:49:43 +0000

    code updated
    need to test on the real hardware

--- codeV2/codeV2.ino
+++ codeV2/codeV2.ino
@@ -1,7 +1,15 @@
 /*
-Adafruit Arduino - Lesson 3. RGB LED
+Airsoft Target System - by Manuel Palmeira
+This system uses piezos and RGY LEDs. For more information on this project and the circuit please check out the
+instructibles page.
+
+Feel free to use and change this code. Just don't use it to exploit others.
+Since I just need simple colors (the 3 primary ones with the LED), I've coded the colors in a ON/OFF base.
+If you want to have more color variations make sure to connect the LEDs to PWM pins and change the values on the
+color functions according to the color that you want.
 */
- 
+
+//setting the pins for the LED interface
 const int Led1yellowPin = 8;
 const int Led1redPin = 7;
 const int Led1greenPin = 6;
@@ -15,27 +23,27 @@ const int Led3greenPin = 0;
 //uncomment this line if using a Common Anode LED
 //#define COMMON_ANODE
 
+//setting the pins for the piezos input.
 const int HeadSensor = A0;
 const int TorsoSensor = A1;
 const int LegSensor = A2;
 
 //Adjust this value to change the sensitivity of the piezos
-const int THRESHOLD = 100;
-//Set this value to the number of microseconds you want each MIDI note to last for
-const int LightsOn = 50;
+const int THRESHOLD = 50;
 
 // these variables will change:
 int HeadSensorReading = 0;      // variable to store the value read from the sensor pin
 int TorsoSensorReading = 0;      // variable to store the value read from the sensor pin
 int LegSensorReading = 0;      // variable to store the value read from the sensor pin
-int MozambiqueCount = 0;      //variable to count the shots required to enter competition mode
-int MozambiqueCountExit = 0;    //variable to count the shots required to exit competition mode
+int RandomON = 0;              //variable to count the shots required to enter competition mode
+int RandomOFF = 0;             //variable to count the shots required to exit competition mode
 int RandomTarget = 0;          //variable to decide the next target
 int RandomTime = 0;          //variable to decide the delay until next random target
 int hit = 0;                //variable to tell if the right target was hit
  
 void setup()
 {
+  //set the LED pins as outputs
   pinMode(Led1yellowPin, OUTPUT);
   pinMode(Led1redPin, OUTPUT);
   pinMode(Led1greenPin, OUTPUT);
@@ -45,17 +53,12 @@ void setup()
   pinMode(Led3yellowPin, OUTPUT);
   pinMode(Led3redPin, OUTPUT);
   pinMode(Led3greenPin, OUTPUT); 
- 
-  Serial.begin(9600);
 }
  
 void loop()
 {
-  /*setColor(1, 0, 0);  // yellow
-  delay(1000);*/
+  //standart mode, all targets are red
   setColor(0, 1, 0);  // red
-  /*setColor(0, 0, 1);  // green
-  delay(1000);*/
   
   // read the sensor and store it in the variable sensorReading:
   HeadSensorReading = analogRead(HeadSensor);
@@ -69,10 +72,8 @@ void loop()
     digitalWrite(Led1redPin, 0);
     digitalWrite(Led1greenPin, 1);
     delay(600);
-    // send the string "Knock!" back to the computer, followed by newline
-    Serial.println("Knock Head!");
-    //one hit for the random mode
-    MozambiqueCount = MozambiqueCount + 1;
+    //  one hit for the random mode
+    RandomON = RandomON + 1;
   }
   
   // if the sensor reading is greater than the threshold:
@@ -82,8 +83,6 @@ void loop()
     digitalWrite(Led2redPin, 0);
     digitalWrite(Led2greenPin, 1);
     delay(600);
-    // send the string "Knock!" back to the computer, followed by newline
-    Serial.println("Knock Torso!");
   }
   
   // if the sensor reading is greater than the threshold:
@@ -93,136 +92,166 @@ void loop()
     digitalWrite(Led3redPin, 0);
     digitalWrite(Led3greenPin, 1);
     delay(600);
-    // send the string "Knock!" back to the computer, followed by newline
-    Serial.println("Knock Leg!");
   }
   
-  if (MozambiqueCount >= 10) {
-    while(MozambiqueCountExit < 10){
+  //  entered random game mode
+  if (RandomON >= 10) {
+    //  you have 10 targets to take out before the game mode ends
+    while(RandomOFF < 10){
       setColor(0, 0, 0);  // lights out
-      RandomTime = random(500, 4000);
-      RandomTarget = random(1, 3);
-      delay(RandomTime);
+      RandomTime = random(500, 4000);  // set a random time to present next target between 500ms and 4s
+      RandomTarget = random(1, 3);  // set a random target
       
       switch (RandomTarget) {
         case 1:
+          delay(RandomTime);
+          hit = 0;  // reset the number of times the target has been hit. you have 3 shots until it moves to the next one
+          //  light the target to hit
           digitalWrite(Led1yellowPin, 1);
           digitalWrite(Led1redPin, 0);
           digitalWrite(Led1greenPin, 0);
-          hit = 0;
           while (hit != 1) {
-            // if the sensor reading is greater than the threshold:
+            // if the target is hit
             if (HeadSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash green lights 3 times
               wright;
               wright;
               wright;
               hit = 1;
             }
-            // if the sensor reading is greater than the threshold:
+            // if you hit the wrong target
             if (TorsoSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash red lights 3 times
               wrong;
               wrong;
               wrong;
               hit = hit + 1; 
             }
-            // if the sensor reading is greater than the threshold:
+            // if you hit the wrong target
             if (LegSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash red lights 3 times
               wrong;
               wrong;
               wrong;
               hit = hit + 1; 
             }
+            //  if the maximum amount of tries has passed, move to the next target
             if (hit = 3) {
+              wrong;
+              wrong;
+              wrong;
               hit = 1; 
             }
           }
+          //  one less target to the end of random mode
+          RandomOFF = RandomOFF + 1;
           break;
         
         case 2:
+          delay(RandomTime);
+          hit = 0;  // reset the number of times the target has been hit. you have 3 shots until it moves to the next one
+          //  light the target to hit
           digitalWrite(Led2yellowPin, 1);
           digitalWrite(Led2redPin, 0);
           digitalWrite(Led2greenPin, 0);
-          hit = 0;
           while (hit != 1) {
-            // if the sensor reading is greater than the threshold:
+            // if the target is hit
             if (TorsoSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash green lights 3 times
               wright;
               wright;
               wright;
               hit = 1;
             }
-            // if the sensor reading is greater than the threshold:
+            // if you hit the wrong target
             if (HeadSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash red lights 3 times
               wrong;
               wrong;
               wrong;
               hit = hit + 1; 
             }
-            // if the sensor reading is greater than the threshold:
+            // if you hit the wrong target
             if (LegSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash red lights 3 times
               wrong;
               wrong;
               wrong;
               hit = hit + 1; 
             }
+            //  if the maximum amount of tries has passed, move to the next target
             if (hit = 3) {
+              wrong;
+              wrong;
+              wrong;
               hit = 1; 
             }
           }
+          //  one less target to the end of random mode
+          RandomOFF = RandomOFF + 1;
           break;
         
         case 3:
+          delay(RandomTime);
+          hit = 0;  // reset the number of times the target has been hit. you have 3 shots until it moves to the next one
+          //  light the target to hit
           digitalWrite(Led3yellowPin, 1);
           digitalWrite(Led3redPin, 0);
           digitalWrite(Led3greenPin, 0);
-          hit = 0;
           while (hit != 1) {
-            // if the sensor reading is greater than the threshold:
+            // if the target is hit
             if (LegSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash green lights 3 times
               wright;
               wright;
               wright;
               hit = 1;
             }
-            // if the sensor reading is greater than the threshold:
+            // if you hit the wrong target
             if (HeadSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash red lights 3 times
               wrong;
               wrong;
               wrong;
               hit = hit + 1;
             }
-            // if the sensor reading is greater than the threshold:
+            // if you hit the wrong target
             if (TorsoSensorReading >= THRESHOLD) {
-              // update the LED pin itself:
+              // flash red lights 3 times
               wrong;
               wrong;
               wrong;
               hit = hit + 1; 
             }
+            //  if the maximum amount of tries has passed, move to the next target
             if (hit = 3) {
+              wrong;
+              wrong;
+              wrong;
               hit = 1; 
             }
           }
+          //  one less target to the end of random mode
+          RandomOFF = RandomOFF + 1;
           break;
         
         default:
-          setColor(1, 0, 0);  // yellow
+          setColor(0, 0, 0);  // lights out
           break;
+      }
+      //  to exit the random mode just shoot any target. each shot here counts for 2 targets
+      if (HeadSensorReading >= THRESHOLD && TorsoSensorReading >= THRESHOLD && LegSensorReading >= THRESHOLD) {
+        setColor(1, 0, 0);  // yellow
+        delay(500);
+        RandomOFF = RandomOFF + 2;
       }
     }
   }
   
   delay(10);  // delay to avoid overloading the serial port buffer
 }
- 
+
+//easy way to set the color on the LEDs 
 void setColor(int yellow, int red, int green)
 {
   #ifdef COMMON_ANODE
@@ -241,6 +270,7 @@ void setColor(int yellow, int red, int green)
   digitalWrite(Led3greenPin, green);
 }
 
+//function to flash green light
 void wright()
 {
   digitalWrite(Led1yellowPin, 0);
@@ -254,6 +284,8 @@ void wright()
   digitalWrite(Led3greenPin, 1);
   delay(100);
 }
+
+//function to flash red light
 void wrong()
 {
   digitalWrite(Led1yellowPin, 0);
